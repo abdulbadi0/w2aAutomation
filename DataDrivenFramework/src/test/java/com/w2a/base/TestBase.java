ba51a82f7cf573d38918329f789f6f9641899b85
Abdul <abdulbadi0@gmail.com>
2022-11-22 22:38:17 +0530

    commit

---
+++ DataDrivenFramework/src/test/java/com/w2a/base/TestBase.java
@@ -0,0 +1,111 @@
+package com.w2a.base;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.NoSuchElementException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.log4j.Logger;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
+import com.w2a.utilities.ExcelReader;
+
+public class TestBase {
+
+	public static WebDriver driver;
+
+	public static Properties config = new Properties();
+	public static Properties OR = new Properties();
+	public static FileInputStream fis;
+	public static Logger log = Logger.getLogger("devpinoyLogger");
+	// PropertyConfigurator.configure("C:\\Users\\Asus\\eclipse-workspace\\DataDrivenFramework\\src\\test\\java\\log4j.properties");
+	public static ExcelReader excel = new ExcelReader(
+			System.getProperty("user.dir") + "\\src\\test\\resources\\excel\\testdata.xlsx");
+
+	@BeforeSuite
+	public void setUP() {
+
+		if (driver == null) {
+			try {
+				fis = new FileInputStream(
+						System.getProperty("user.dir") + "\\src\\test\\resources\\properties\\Config.properties");
+			} catch (FileNotFoundException e) {
+
+				e.printStackTrace();
+			}
+
+			try {
+				config.load(fis);
+				log.debug("config file loaded !!!");
+			} catch (IOException e) {
+
+				e.printStackTrace();
+			}
+
+			try {
+				fis = new FileInputStream(
+						System.getProperty("user.dir") + "\\src\\test\\resources\\properties\\OR.properties");
+			} catch (FileNotFoundException e) {
+
+				e.printStackTrace();
+			}
+
+			try {
+				OR.load(fis);
+				log.debug("OR file loaded !!!");
+			} catch (IOException e) {
+
+				e.printStackTrace();
+			}
+
+			if (config.getProperty("browser").equals("firefox")) {
+				System.setProperty("webdriver.gecko.driver", "gecko.exe");
+				driver = new FirefoxDriver();
+			} else if (config.getProperty("browser").equals("chrome")) {
+				System.setProperty("webdriver.chrome.driver",
+						System.getProperty("user.dir") + "\\src\\test\\resources\\executables\\chromedriver.exe");
+				driver = new ChromeDriver();
+				log.debug("Chrome Launched !!!");
+			} else if (config.getProperty("browser").equals("IE")) {
+				System.setProperty("webdriver.ie.driver",
+						System.getProperty("user.dir") + "\\src\\test\\resources\\executables\\IE.exe");
+				driver = new InternetExplorerDriver();
+			}
+
+			driver.get(config.getProperty("testsiteurl"));
+			log.debug("Navigated to " + config.getProperty("testsiteurl"));
+			driver.manage().window().maximize();
+			driver.manage().timeouts().implicitlyWait(Integer.parseInt(config.getProperty("implicit.wait")),
+					TimeUnit.SECONDS);
+		}
+
+	}
+
+	public boolean isElementPresent(By by) {
+
+		try {
+			driver.findElement(by);
+			return true;
+		} catch (NoSuchElementException e) {
+			return false;
+		}
+	}
+
+	@AfterSuite
+	public void tearDown() {
+
+		if (driver != null) {
+			driver.quit();
+		}
+		log.debug("Test execution completed.");
+	}
+
+}
---
+++ DataDrivenFramework/src/test/java/com/w2a/listeners/Dummy.java
@@ -0,0 +1,8 @@
+package com.w2a.listeners;
+
+public class Dummy {
+	public static void main(String[] args) {
+		System.out.println("Hello");
+	}
+
+}
